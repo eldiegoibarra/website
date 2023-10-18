bde12e6d24af2924a103b3be7e3c672e3829e867
Diego Ibarra-Asiain <123170415+eldiegoibarra@users.noreply.github.com>
2023-10-18 17:42:34 +0100

    small changes

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-/* const icons = document.querySelectorAll(".social-icon");
-
-icons.forEach((icon) => {
-  icon.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    const target = this.getAttribute("data-target");
-    const content = document.getElementById(target);
-
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}); */
-
-
 const icons = document.querySelectorAll(".social-icon");
 
 icons.forEach((icon) => {
